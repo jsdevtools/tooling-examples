ac65221d8284582c85d64d2f54405b71eb25e771
Justin Staubach <justin@staubach.us>
2019-09-17 19:59:56 -0400

    redirect to index.html

---
+++ html-15/nil/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// const https = require('https');
+const http = require('http');
+
+const app = express();
+
+const options = {
+};
+
+app.get('/', function(req, res) {
+  res.redirect('/index.html');
+});
+
+http.createServer(options, app).listen(process.env.PORT || 8080);
