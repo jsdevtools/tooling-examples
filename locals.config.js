207f61fb3e1f3d56d42d23805accb08c7e1d05b8
Justin Staubach <justin@staubach.us>
2019-09-21 19:28:01 -0400

    add ext to nav properties.

--- locals.config.js
+++ locals.config.js
@@ -2,7 +2,7 @@ const topics = require('./locals');
 module.exports = {
   topics,
   nav: (({favorites, ...rest}) => ([
-    { title: favorites.title, filename: favorites.url },
+    { title: favorites.title, filename: favorites.url, ext: favorites.ext },
     ...Object.entries(rest)
       .sort(([a,],[b,],) => (b < a))
       .map(([key,value])=>({ title: value.title, filename: value.url, ext: value.ext })),
