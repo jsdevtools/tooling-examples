7be334b0eb49a064f6ff124f0900cc14e6eed236
Justin Staubach <justin@staubach.us>
2019-09-23 19:48:50 -0400

    initial commit of html-16

---
+++ html-16/fetchData.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const fetchNav = async () => {
+  try {
+    const response = await axios.get('/nav', {
+      baseURL: 'https://html-16-api.jsdevtools.com',
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const fetchTopics = async () => {
+  try {
+    const response = await axios.get('/topics', {
+      baseURL: 'https://html-16-api.jsdevtools.com',
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+(async () => {
+  const nav = await fetchNav();
+  const topics = await fetchTopics();
+  console.log(JSON.stringify({ topics, nav }));
+  fs.writeFile(
+    './locals.config.js',
+    `module.exports = ${JSON.stringify({ topics, nav }, null, 2)}`,
+    'utf-8',
+    (err) => {
+      if (err) throw err;
+    }
+  );
+})();
