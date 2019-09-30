51917422d4d6829b21eec90db543de0b165398c5
Justin Staubach <justin@staubach.us>
2019-09-29 21:25:15 -0400

    Initial commit of html-19,-20

---
+++ html-19/fetchData.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { fetchTopics, fetchNav } = require('fetch-graphql');
+
+const config = {
+  dataSource: 'REST_FILE',
+  // uri: 'http://localhost:4000',
+  uri: 'https://html-api-graphql.jsdevtools.com',
+};
+
+(async () => {
+  const topics = await fetchTopics(config);
+  const nav = await fetchNav(config);
+
+  console.log(JSON.stringify({topics, nav}));
+  fs.writeFile(
+    './locals.config.js',
+    `module.exports = ${JSON.stringify({topics, nav}, null, 2)}`,
+    'utf-8',
+    (err) => {
+      if (err) throw err;
+    }
+  );
+})();
