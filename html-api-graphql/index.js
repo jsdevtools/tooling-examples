ea53e52afe90e396ddf39a0650dbe9fc2aaf19c3
Justin Staubach <justin@staubach.us>
2019-09-29 22:40:25 -0400

    Initial commit of html-21

--- html-api-graphql/index.js
+++ html-api-graphql/index.js
@@ -2,12 +2,14 @@ const { ApolloServer, gql } = require('apollo-server');
 const locals = require('../locals.config.js');
 const { fetchTopics, fetchNav } = require('fetch-file');
 const { fetchLocals } = require('fetch-pg');
+const { pool } = require('./config');
 
 const typeDefs = gql`
   enum BackEndDataSource {
     LOCAL_FILE
     REST_FILE
     REST_PG
+    PG
   }
 
   type Topics {
@@ -46,10 +48,15 @@ const resolvers = {
     LOCAL_FILE: 'localFile',
     REST_FILE: 'restFile',
     REST_PG: 'restPg',
+    PG: 'pg',
   },
   Query: {
     topics: async (_, args) => {
       switch(args.backEndDataSource) {
+        case 'pg': {
+          const { rows } = await pool.query('SELECT * FROM locals ORDER BY id LIMIT 1');
+          return rows[0].info.topics; 
+        }
         case 'restPg': {
           const tempTopics = await fetchLocals();
           return tempTopics.topics;
@@ -65,6 +72,17 @@ const resolvers = {
     },
     nav: async (_, args) => {
       switch(args.backEndDataSource) {
+        case 'pg': {
+          const { rows } = await pool.query('SELECT * FROM locals ORDER BY id LIMIT 1');
+          const tempTopics =  rows[0].info; 
+          const {favorites, ...rest} = tempTopics.topics;
+          return [
+            { title: favorites.title, filename: favorites.url, ext: favorites.ext },
+            ...Object.entries(rest)
+              .sort(([a,],[b,],) => (b < a))
+              .map(([key,value])=>({ title: value.title, filename: value.url, ext: value.ext })),
+          ];
+        }
         case 'restPg': {
           const tempTopics = await fetchLocals();
           const {favorites, ...rest} = tempTopics.topics;
